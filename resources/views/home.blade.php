e0a4c5164356a482344b653cbcfaf7bac05e28d4
Blessybunny <sonicgenerators@gmail.com>
2021-05-23 01:22:54 +0800

    misc

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -76,16 +76,16 @@
         
     </style>
     <!-- CAROUSEL -->
-    <div id = "home-carousel" class = "carousel">
-        <div id = "carouselExampleIndicators" class = "carousel slide" data-ride = "carousel" data-interval = "5000">
+    <div id = "home-carousel">
+        <div id = "homeCarouselIndicators" class = "carousel slide" data-ride = "carousel" data-interval = "5000">
             <ol class = "carousel-indicators">
-                <li data-target = "#carouselExampleIndicators" data-slide-to = "0" class = "active"></li>
-                <li data-target = "#carouselExampleIndicators" data-slide-to = "1"></li>
-                <li data-target = "#carouselExampleIndicators" data-slide-to = "2"></li>
+                <li data-target = "#homeCarouselIndicators" data-slide-to = "0" class = "active"></li>
+                <li data-target = "#homeCarouselIndicators" data-slide-to = "1"></li>
+                <li data-target = "#homeCarouselIndicators" data-slide-to = "2"></li>
             </ol>
             <div class = "carousel-inner">
                 <div class = "carousel-item active" style = "background-image: url('./img/bg-1.jpg')">
-                    <img class = "d-block w-100" src = "./img/bg-1.jpg" alt = "First slide">
+                    <img class = "d-block w-100" src = "./img/bg-1.jpg">
                     <div class = "carousel-caption d-none d-md-block">
                         <h1>Welcome to Opalescence</h1>
                         <h4>Home to your casual fashion needs.</h4>
@@ -93,23 +93,23 @@
                     </div>
                 </div>
                 <div class = "carousel-item" style = "background-image: url('./img/bg-2.jpg')">
-                    <img class = "d-block w-100" src = "./img/bg-2.jpg" alt = "Second slide">
+                    <img class = "d-block w-100" src = "./img/bg-2.jpg">
                     <div class = "carousel-caption d-none d-md-block">
                         <h4>"Style is a way to say who you are without having to speak."</h4>
                     </div>
                 </div>
                 <div class = "carousel-item" style = "background-image: url('./img/bg-3.jpg')">
-                    <img class = "d-block w-100" src = "./img/bg-3.jpg" alt = "Third slide">
+                    <img class = "d-block w-100" src = "./img/bg-3.jpg">
                     <div class = "carousel-caption d-none d-md-block">
                         <h4>"Clothes mean nothing until someone lives in them."</h4>
                     </div>
                 </div>
             </div>
-            <a class = "carousel-control-prev" href = "#carouselExampleIndicators" role = "button" data-slide = "prev">
+            <a class = "carousel-control-prev" href = "#homeCarouselIndicators" role = "button" data-slide = "prev">
                 <i class = "material-icons">keyboard_arrow_left</i>
                 <span class = "sr-only">Previous</span>
             </a>
-            <a class = "carousel-control-next" href = "#carouselExampleIndicators" role = "button" data-slide = "next">
+            <a class = "carousel-control-next" href = "#homeCarouselIndicators" role = "button" data-slide = "next">
                 <i class = "material-icons">keyboard_arrow_right</i>
                 <span class = "sr-only">Next</span>
             </a>
@@ -118,6 +118,86 @@
 
     <!-- MAIN CONTENT -->
     <div class = "main">
+        
+        <!-- New clothing lines -->
+        <div class = "clothing-line-container container">
+            <div class = "row">
+                <div class = "col-md-12">
+                    <h3 class = "header-3">Latest Apparels</h3>
+                    <hr/>
+                </div>
+            </div>
+            <div class = "row">
+                <div class = "news-card col-sm-3 col-md-6 col-lg-6 col-xl-3">
+                    <div class = "inner">
+                        <div id = "clothingLineCarousel1Indicator" class = "carousel slide" data-ride = "carousel" data-interval = "3000">
+                            <ol class = "carousel-indicators">
+                                <li data-target = "#clothingLineCarousel1Indicator" data-slide-to = "0" class = "active"></li>
+                                <li data-target = "#clothingLineCarousel1Indicator" data-slide-to = "1"></li>
+                                <li data-target = "#clothingLineCarousel1Indicator" data-slide-to = "2"></li>
+                                <li data-target = "#clothingLineCarousel1Indicator" data-slide-to = "3"></li>
+                            </ol>
+                            <div class = "carousel-inner">
+                                <div class = "carousel-item active">
+                                    <img class = "d-block w-100" src = "./img/cloth-1.jpg">
+                                </div>
+                                <div class = "carousel-item">
+                                    <img class = "d-block w-100" src = "./img/cloth-2.jpg">
+                                </div>
+                                <div class = "carousel-item">
+                                    <img class = "d-block w-100" src = "./img/cloth-3.jpg">
+                                </div>
+                                <div class = "carousel-item">
+                                    <img class = "d-block w-100" src = "./img/cloth-4.jpg">
+                                </div>
+                            </div>
+                            <a class = "carousel-control-prev" href = "#clothingLineCarousel1Indicator" role = "button" data-slide = "prev">
+                                <i class = "material-icons">keyboard_arrow_left</i>
+                                <span class = "sr-only">Previous</span>
+                            </a>
+                            <a class = "carousel-control-next" href = "#clothingLineCarousel1Indicator" role = "button" data-slide = "next">
+                                <i class = "material-icons">keyboard_arrow_right</i>
+                                <span class = "sr-only">Next</span>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+                <div class = "news-card col-sm-3 col-md-6 col-lg-6 col-xl-3">
+                    <div class = "inner">
+                        <div id = "clothingLineCarousel2Indicator" class = "carousel slide" data-ride = "carousel" data-interval = "3000">
+                            <ol class = "carousel-indicators">
+                                <li data-target = "#clothingLineCarousel2Indicator" data-slide-to = "0" class = "active"></li>
+                                <li data-target = "#clothingLineCarousel2Indicator" data-slide-to = "1"></li>
+                                <li data-target = "#clothingLineCarousel2Indicator" data-slide-to = "2"></li>
+                                <li data-target = "#clothingLineCarousel2Indicator" data-slide-to = "3"></li>
+                            </ol>
+                            <div class = "carousel-inner">
+                                <div class = "carousel-item active">
+                                    <img class = "d-block w-100" src = "./img/cloth-5.jpg">
+                                </div>
+                                <div class = "carousel-item">
+                                    <img class = "d-block w-100" src = "./img/cloth-6.jpg">
+                                </div>
+                                <div class = "carousel-item">
+                                    <img class = "d-block w-100" src = "./img/cloth-7.jpg">
+                                </div>
+                                <div class = "carousel-item">
+                                    <img class = "d-block w-100" src = "./img/cloth-8.jpg">
+                                </div>
+                            </div>
+                            <a class = "carousel-control-prev" href = "#clothingLineCarousel2Indicator" role = "button" data-slide = "prev">
+                                <i class = "material-icons">keyboard_arrow_left</i>
+                                <span class = "sr-only">Previous</span>
+                            </a>
+                            <a class = "carousel-control-next" href = "#clothingLineCarousel2Indicator" role = "button" data-slide = "next">
+                                <i class = "material-icons">keyboard_arrow_right</i>
+                                <span class = "sr-only">Next</span>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
         
         <!-- News cards -->
         <div class = "news-container container">
@@ -139,7 +219,7 @@
                 <div class = "news-card col-sm-6 col-md-4 col-lg-4 col-xl-3">
                     <div class = "inner">
                         <img src = "./img/bg-2.jpg"/>
-                        <h4>Prideful Fashion 2 →</h4>
+                        <h4>Prideful Fashion →</h4>
                         <h6>May 22, 2021</h6>
                         <p>Find ways to still show off your fashion tastes during lockdowns.</p>
                     </div>
@@ -163,7 +243,6 @@
             </div>
         </div>
         
-        
         <!-- Span 1 -->
         <div class = "section section-basic">
             <div class = "container">
@@ -175,7 +254,7 @@
                     <li>Add a "featured" card for trending clothes to this landing page.</li>
                     <li>Steal ideas from other existing fashion sites?</li>
                     <li>Wallpapers should be 16:9 horizontal.</li>
-                    <li>Items should be 16:9 vertical.</li>
+                    <li>Items should be  5:7 vertical.</li>
                 </ul>
             </div>
         </div>
