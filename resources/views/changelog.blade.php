bcd86af8abec598f17cf975e02ae0604897c4bb2
Blessybunny <sonicgenerators@gmail.com>
2021-05-22 15:55:19 +0800

    misc
    changelog

---
+++ resources/views/changelog.blade.php
@@ -0,0 +1,66 @@
+@extends('/layouts/master')
+
+@section('content')
+
+    <!-- SPLASH HEADER -->
+    <div class = "page-header header-filter clear-filter purple-filter" data-parallax = "true" style = "background-image: url('./img/bg-001.jpg');">
+        <div class = "container">
+            <div class = "row">
+                <div class = "col-md-8 ml-auto mr-auto">
+                    <div class = "brand">
+                        <h1>CHANGELOG</h1>
+                        <h3>A template is used. If you read this, just ignore everything in the body.</h3>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>         
+
+    <!-- CHANGELOG -->
+    <div class = "main main-raised">
+        <div class = "section section-tabs">
+            <div class = "container">
+                <div id = "nav-tabs">
+                    <h3>Changelog</h3>
+                    <div class = "row">
+                        <div class = "col-md-12">
+                            <div class = "card card-nav-tabs card-plain">
+                                <div class = "card-header card-header-primary">
+                                    <div class = "nav-tabs-navigation">
+                                        <div class = "nav-tabs-wrapper">
+                                            <ul class = "nav nav-tabs" data-tabs = "tabs">
+                                                <li class = "nav-item">
+                                                    <a class = "nav-link active" href = "#y-2021" data-toggle = "tab">2021</a>
+                                                </li>
+                                                <li class = "nav-item">
+                                                    <a class = "nav-link" href = "#y-2022" data-toggle = "tab">2022</a>
+                                                </li>
+                                            </ul>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class = "card-body">
+                                    <div class = "tab-content">
+                                        <div class = "tab-pane active" id = "y-2021">
+                                            <ul>
+                                                <h4>May 22</h4>
+                                                <li><a href = "https://www.creative-tim.com/product/material-kit-pro?partner=114912">Material kit</a> template implemented</li>
+                                                <li>Changelog added.</li>
+                                            </ul>
+                                            <ul>
+                                                <h4>May 21</h4>
+                                                <li>Deployment of this website.</li>
+                                            </ul>
+                                        </div>
+                                        <div class="tab-pane" id = "y-2022"></div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -13,10 +13,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('home');
-});
+//NAVIGATION
+    Route::get('/', function () {return view('home');});
+    Route::get('/home', function () {return redirect('/');});
+    Route::get('/changelog', function () {return view('changelog');});
 
-Route::get('/home', function () {
-    return view('home');
-});
