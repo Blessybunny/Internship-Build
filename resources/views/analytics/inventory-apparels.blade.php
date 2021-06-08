cbd6e26c7b1a6e9a8bfd97113b71c02f920c6c98
Blessybunny <sonicgenerators@gmail.com>
2021-06-08 16:27:48 +0800

    misc

--- resources/views/analytics/inventory-apparels.blade.php
+++ resources/views/analytics/inventory-apparels.blade.php
@@ -15,6 +15,7 @@
         <div class = "container-fluid">
             <div class = "row">
                 <div class = "col">
+                    <!-- Apparel - Shirts -->
                     <h3>Apparels - Shirts</h3>
                     <hr/>
                     <table class = "table table-shopping">
@@ -34,7 +35,7 @@
                         <tbody>
                             @foreach ($apparels as $apparel)
                                 @if ($apparel->type === "shirt")
-                                    <tr>
+                                    <tr onclick = "popModal({{ $apparel->id }}, '{{ $apparel->name }}', {{ $apparel->price }}, '{{ $apparel->type }}', {{ $apparel->stock_universal }}, [{{ $apparel->stock_xs }}, {{ $apparel->stock_s }}, {{ $apparel->stock_m }}, {{ $apparel->stock_l }}, {{ $apparel->stock_xl }}])" data-toggle = "modal" data-target = "#apparelModal">
                                         <td>{{ $apparel->name }}</td>
                                         <td>PHP {{ $apparel->price }}</td>
                                         <td class = "text-center @if ($apparel->stock_xs >= 40) green @elseif ($apparel->stock_xs >= 20) black @elseif ($apparel->stock_xs >= 10) orange @else red @endif">{{ $apparel->stock_xs }}</td>
@@ -49,6 +50,8 @@
                             @endforeach
                           </tbody>
                     </table>
+                    
+                    <!-- Apparel - Accessories -->
                     <h3>Apparels - Accessories</h3>
                     <hr/>
                     <table class = "table table-shopping">
@@ -63,7 +66,7 @@
                         <tbody>
                             @foreach ($apparels as $apparel)
                                 @if ($apparel->type === "accessory")
-                                    <tr>
+                                    <tr onclick = "popModal({{ $apparel->id }}, '{{ $apparel->name }}', {{ $apparel->price }}, '{{ $apparel->type }}', {{ $apparel->stock_universal }}, [{{ $apparel->stock_xs }}, {{ $apparel->stock_s }}, {{ $apparel->stock_m }}, {{ $apparel->stock_l }}, {{ $apparel->stock_xl }}])" data-toggle = "modal" data-target = "#apparelModal">
                                         <td>{{ $apparel->name }}</td>
                                         <td>PHP {{ $apparel->price }}</td>
                                         <td class = "text-center @if ($apparel->stock_universal >= 40) green @elseif ($apparel->stock_universal >= 20) black @elseif ($apparel->stock_universal >= 10) orange @else red @endif">{{ $apparel->stock_universal }}</td>
@@ -73,6 +76,82 @@
                             @endforeach
                           </tbody>
                     </table>
+                    
+                    <!-- Apparel - Modals -->
+                    <script>
+                        function popModal (id, name, price, type, stockUniversal, stocks) {
+                            //Print name and price
+                            document.getElementById(`modal-apparel-name`).innerHTML = name;
+                            document.getElementById(`modal-apparel-price`).innerHTML = price;
+                            
+                            //Variables
+                            let modalQuantity = document.getElementById(`modal-quantity`);
+                            modalQuantity.innerHTML = ``;
+                            
+                            //Quantities for shirts
+                            if (type === `shirt`) {
+                                let sizes = [`extra small`, `small`, 'medium', `large`, `extra large`];
+                                for (let i = 0, ii = stocks.length; i < ii; i++) {
+                                    let paragraph = document.createElement(`p`);
+                                    if (stocks[i] >= 40) {
+                                        paragraph.innerHTML = `This product is too high in stock of ${sizes[i]} sizes (quantity: ${stocks[i]}, maximum of 40 recommended). Consider halting production.`;
+                                    }
+                                    else if (stocks[i] >= 20) {
+                                        paragraph.innerHTML = `This product has an optimal amount of stocks in ${sizes[i]} sizes (quantity: ${stocks[i]}).`;
+                                    }
+                                    else if (stocks[i] >= 10) {
+                                        paragraph.className = `orange`;
+                                        paragraph.innerHTML = `This product is low in stock of ${sizes[i]} sizes (quantity: ${stocks[i]}, minimum of 20 recommended).`;
+                                    }
+                                    else {
+                                        paragraph.className = `red`;
+                                        paragraph.innerHTML = `This product is very low in stock of ${sizes[i]} sizes (quantity: ${stocks[i]}, minimum of 20 recommended).`;
+                                    }
+                                    modalQuantity.appendChild(paragraph);
+                                }
+                            }
+                            
+                            //Quantities for accessories
+                            else if (type === `accessory`) {
+                                let paragraph = document.createElement(`p`);
+                                if (stockUniversal >= 40) {
+                                    paragraph.innerHTML = `This product is too high in stock (quantity: ${stockUniversal}, maximum of 40 recommended). Consider halting production.`;
+                                }
+                                else if (stockUniversal >= 20) {
+                                    paragraph.innerHTML = `This product has an optimal amount of stock (quantity: ${stockUniversal}).`;
+                                }
+                                else if (stockUniversal >= 10) {
+                                    paragraph.className = `orange`;
+                                    paragraph.innerHTML = `This product is low in stock (quantity: ${stockUniversal}, minimum of 20 recommended).`;
+                                }
+                                else {
+                                    paragraph.className = `red`;
+                                    paragraph.innerHTML = `This product is very low in stock (quantity: ${stockUniversal}, minimum of 20 recommended).`;
+                                }
+                            }
+                        }
+                    </script>
+                    <div class = "modal fade" id = "apparelModal">
+                        <div class = "modal-dialog modal-lg">
+                            <div class = "modal-content">
+                                <div class = "modal-header">
+                                    <h4 id = "modal-apparel-name" class = "modal-title"></h4>
+                                    <button type = "button" class = "close" data-dismiss = "modal" aria-label = "Close">
+                                        <span aria-hidden = "true">&times;</span>
+                                    </button>
+                                </div>
+                                <div class = "modal-body">
+                                    <p>Price: PHP <a id = "modal-apparel-price"></a></p>
+                                    <p>Notes: </p>
+                                    <div id = "modal-quantity"></div>
+                                </div>
+                                <div class = "modal-footer">
+                                    <button type = "button" class = "btn" data-dismiss = "modal">Close</button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    
                 </div>
             </div>
         </div>
